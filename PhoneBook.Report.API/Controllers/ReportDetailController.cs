0e80c4b5a1bf907d3d9800aea5d838cc84dec03c
Kartal <eneskartal117@gmail.com>
2023-07-30 21:10:53 +0300

    API endpoints prepared for services.

---
+++ PhoneBook.Report.API/Controllers/ReportDetailController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneBook.Report.API.Services;
+
+namespace PhoneBook.Report.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/reportdetail")]
+    public class ReportDetailController : ControllerBase
+    {
+        private readonly IReportDetailService reportDetailService;
+        public ReportDetailController(IReportDetailService reportDetailService)
+        {
+            this.reportDetailService = reportDetailService;
+        }
+
+        [HttpGet("Get/{reportId}")]
+        public async Task<IActionResult> Get(string reportId)
+        {
+            Guid record_id;
+            if (!Guid.TryParse(reportId, out record_id))
+                return BadRequest("Invalid id");
+
+            var result = await reportDetailService.GetByReportIdAsync(record_id);
+            return Ok(result);
+        }
+    }
+}
