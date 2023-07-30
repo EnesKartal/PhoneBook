e37a10b3e27277325cae848a78c9de1c1ac24b79
Kartal <eneskartal117@gmail.com>
2023-07-30 21:13:15 +0300

    Services injected and extra undefined actions added

--- PhoneBook.Report.API/Program.cs
+++ PhoneBook.Report.API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PhoneBook.Report.API.Models.Domain;
 using PhoneBook.Report.API.Repositories;
+using PhoneBook.Report.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,9 @@ builder.Services.AddControllers();
 
 builder.Services.AddScoped<IReportRepository, ReportRepository>();
 builder.Services.AddScoped<IReportDetailRepository, ReportDetailRepository>();
+
+builder.Services.AddScoped<IReportService, ReportService>();
+builder.Services.AddScoped<IReportDetailService, ReportDetailService>();
 
 builder.Services.AddDbContext<PhoneBookReportDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PhoneBookReportConnectionString")));
